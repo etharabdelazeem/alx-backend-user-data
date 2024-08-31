3ffbb4d08577476567a0c7f95e5147fbce7f6c63
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-08-31 11:46:07 +0000

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,9 +1,19 @@
+#!/usr/bin/env python3
+"""
+Encrypting passwords
+"""
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
-    """Hash a password with bcrypt and return the hashed password as a byte string."""
+    """Hash a password with bcrypt and return the hashed password"""
     # Generate a salt
     salt = bcrypt.gensalt()
     # Hash the password with the generated salt
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if the provided password matches the hashed password."""
+    return bcrypt.checkpw(password.encode(), hashed_password)
