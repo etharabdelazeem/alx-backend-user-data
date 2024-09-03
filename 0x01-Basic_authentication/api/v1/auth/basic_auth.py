3d3c0eee49f9f0cfd41dcf693d1f5a7f7f5ccc94
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-09-03 19:18:47 +0000

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,17 @@ class BasicAuth(Auth):
     """
     Implementation of Basic authentication class
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header for a Basic
+        Authorization
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        token = authorization_header.split(" ")[-1]
+        return token
