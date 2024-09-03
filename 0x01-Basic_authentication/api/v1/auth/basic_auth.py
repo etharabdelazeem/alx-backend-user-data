3e348219fab875338db005af8739f5b9c6089879
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-09-03 19:11:43 +0000

    task 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic Authentication class definition
+"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Implementation of Basic authentication class
+    """
