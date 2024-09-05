359327783c90e27ee86bf7afacd4cbd095f40050
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-09-05 22:33:45 +0000

    task 5 style error fixed

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -42,7 +42,7 @@ def before_request():
                 ]
         if auth.require_auth(request.path, excluded):
             if auth.authorization_header(request) is None and\
-                auth.session_cookie(request) is None:
+                    auth.session_cookie(request) is None:
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
                 abort(403, description="Forbidden")
